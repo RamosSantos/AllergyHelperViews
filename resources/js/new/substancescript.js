1d3a272aa9a7d7485c037052b44cf44f6133af59
Felipe Ramos dos Santos <f.ramossantos@gmail.com>
2016-03-31 12:03:25 -0300

    add modal
    add modal

--- resources/js/new/substancescript.js
+++ resources/js/new/substancescript.js
@@ -62,10 +62,19 @@ $(function() {
 
 
 var app = angular.module("substaciesApp", ["firebase"]);
-app.controller("substanciesCrtl", function($scope, $firebaseArray) {
+app.controller("substanciesCrtl", function($scope, $firebaseObject,$timeout) {
     var ref = new Firebase("https://allergyhelper3.firebaseio.com/substancies");
-    $scope.dataList = $firebaseArray(ref);
-
+    var obj = $firebaseObject(ref);
+	$scope.loader = true;
+	
+	$scope.dataList = obj;
+	
+	$timeout(function(){
+		$scope.$apply(function(){
+			$scope.loader=false;
+		});
+	},1000);
+	
     $scope.submitForm = function(){
         var casnum = $scope.casnum1+$scope.casnum2+$scope.casnum3;       
     };
@@ -73,12 +82,10 @@ app.controller("substanciesCrtl", function($scope, $firebaseArray) {
     
     $scope.editItem = function(argElement) {
         var element = argElement.target;
-        $(element).closest('td').next('td').find('button').removeAttr('disabled');
-        var name = $(element).closest('td').prev('td').prev('td').children();
-        var desc = $(element).closest('td').prev('td').prev('td').prev('td').children();
-        name.removeAttr('disabled');
-        name.addClass('form-control').removeClass('input-table-disable');
-        desc.removeAttr('disabled');
-        desc.addClass('form-control').removeClass('input-table-disable');      
+		element = $(element).parent().parent().closest('tr');
+        $(element).find("td").each(function(){
+			$(this).children().removeAttr('disabled');
+			$(this).children("input[type='text']").removeClass('input-table-disable').addClass('form-control');
+		})
     };
 });
