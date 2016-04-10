83f5f85ce3deb9d103e3657b5ca7ad9311a3495f
Danilo de Queiroz <danilo@danilo.nl>
2016-04-09 23:29:39 -0300

    remigrated to firebaseArray , ng-otions orderBy.

--- resources/js/new/substancescript.js
+++ resources/js/new/substancescript.js
@@ -43,6 +43,17 @@ $(function() {
     });
 });
 
+function findWithAttr(array, attr, value) {
+    var indexes = [];
+    if (value.length > 0 && attr !== '') {
+        for (var i = 0; i < array.length; i += 1) {
+            if ((value.indexOf(array[i][attr]) > -1)) {
+                indexes.push(i);
+            }
+        }
+    }
+    return indexes;
+}
 
 var app = angular.module("substaciesApp", ["firebase", "fxpicklist"]);
 app.controller("substanciesCrtl", function($scope, $firebaseArray, $firebaseObject, $timeout) {
@@ -52,7 +63,7 @@ app.controller("substanciesCrtl", function($scope, $firebaseArray, $firebaseObje
     $scope.loader = true;
 
     $scope.substList = arr;
-    $scope.substMap = obj;
+
     $scope.testCtrl1 = function() {
 
     };
@@ -78,160 +89,71 @@ app.controller("substanciesCrtl", function($scope, $firebaseArray, $firebaseObje
     };
 
     $scope.copyOne = function() {
-        var picked = $scope.rSubstances;
-        var picked = $scope.rSelected;
-        $scope.toPickMap
+        var pickedIds = $scope.rSubstances;
+        var pickedPositions = findWithAttr($scope.substMap, '$id', pickedIds);
+        for (var i = pickedPositions.length - 1; i >= 0; i--) {
+            $scope.toPickMap.push($scope.substMap[pickedPositions[i]]);
+            $scope.substMap.splice([pickedPositions[i]], 1);
+        }
     };
 
     $scope.removeOne = function() {
-        alert($scope.lPicked);
+        var pickedIds = $scope.lPicked;
+        var pickedPositions = findWithAttr($scope.toPickMap, '$id', pickedIds);
+        for (var i = pickedPositions.length - 1; i >= 0; i--) {
+            $scope.substMap.push($scope.toPickMap[pickedPositions[i]]);
+            $scope.toPickMap.splice([pickedPositions[i]], 1);
+        }
     };
-
-
-
-
-    // function getModelsByIds(keys) {
-    //     var models = [];
-    //     $("#modalSimSelect").select2("data", [{ "id": "2127", "text": "Henry Ford" }, { "id": "2199", "text": "Tom Phillips" }]);
-    //     var promisses = [];
-    //     for (var i in keys) {
-    //         var ref = new Firebase('https://allergyhelper3.firebaseio.com/substancies/' + keys[i]);
-    //         var obj = $firebaseObject(ref);
-    //         promisses.push(obj.$loaded());
-    //     }
-    //     Promise.all(promisses).then(function(values) {
-    //         console.log(values); // [3, 1337, "foo"] 
-
-    //     });
-
-    // }
 
     $scope.selectModal = function(argElement) {
         var promisses = [];
         var data = [];
         var currentSimilars;
-        var list = $scope.substMap;
-        $scope.toPickMap = {};
-        $scope.substMap = list;
+        $scope.toPickMap = [];
+        $scope.substMap  = [].concat($scope.substList);
         if (this.substItem.similarTo !== undefined) {
             currentSimilars = this.substItem.similarTo;
             var keys = $.map(currentSimilars, function(v, i) {
                 return i;
             });
-            for (var j = 0; j < keys.length; j++) {
-                if (keys[j] in list) {
-                    $scope.toPickMap[keys[j]] = list[keys[j]];
-                    delete $scope.substMap[keys[j]];
-                }
+            var blah = findWithAttr($scope.substMap, '$id', keys);
+            for (var i = blah.length - 1; i >= 0; i--) {
+                $scope.toPickMap.push($scope.substMap[blah[i]]);
+                $scope.substMap.splice(blah[i], 1);
             }
         }
-        // var populated = new Array();
-        // if (this.substItem.similarTo !== undefined) {
-        //     currentSimilars = this.substItem.similarTo;
-        //     var keys = $.map(currentSimilars, function(v, i) {
-        //         return i;
-        //     });
-        //     for (var i = 0; i < list.length; i++) {
-        //         for (var j = 0; j < keys.length; j++) {
-        //             if (keys[j] === list[i].$id) {
-        //                 $scope.lSetOrChoosen.push($scope.rAvailable[i]);
-        //                 $scope.rAvailable.splice( i, 1 );
-        //             }
-        //         }
-        //     }
-        // }
-        // $scope.lSetOrChoosen=populated;
-        // for(var i=0; i<10; i++){
-        //     $scope.rAvailable.push({
-        //         name: " display name"+i,
-        //         value: "value"+i,
-        //         index: i
-        //     });
-        // }
+    };
 
-        // $scope.lSetOrChoosen=[$scope.rAvailable[4].value, $scope.rAvailable[5].value];
+    $scope.addSubstance = function() {
+        var substance = $scope.substance;
+        substance.lowerCaseName = substance.commonName.toLowerCase();
+        var arrSimilars = $(".subst-select").val();
+        var arrSimilars2 = $(".similarSelect").select2('data');
+        var arrSimilars3 = $("#similarSelect").select2('data');
 
-        // var keys = $.map(item, function(v, i) {
-        //     return i;
-        // });
+        var similarTo = {};
 
-        $scope.addSubstance = function() {
-            var substance = $scope.substance;
-            substance.lowerCaseName = substance.commonName.toLowerCase();
-            var arrSimilars = $(".subst-select").val();
-            var arrSimilars2 = $(".similarSelect").select2('data');
-            var arrSimilars3 = $("#similarSelect").select2('data');
-
-            var similarTo = {};
-
-            if (arrSimilars !== null) {
-                arrSimilars.forEach(function(item) {
-                    similarTo[item] = true;
-                    substance.hasSimilarTo = true;
-                });
-                substance.similarTo = similarTo;
-            } else {
-                substance.isSimilar = false;
-            }
-            $scope.dataList.$add(substance).then(function() {
-                $("#messages").html("Salvo com sucesso").fadeIn(function() {
-                    $(this).fadeOut(3000);
-                    $scope.substance = "";
-                });
+        if (arrSimilars !== null) {
+            arrSimilars.forEach(function(item) {
+                similarTo[item] = true;
+                substance.hasSimilarTo = true;
             });
-
-        };
-
-        $("#modalSimSelect").select2({
-            initSelection: function(element, callback) {
-                for (var i in keys) {
-                    var ref = new Firebase('https://allergyhelper3.firebaseio.com/substancies/' + keys[i]);
-                    var obj = $firebaseObject(ref);
-                    promisses.push(obj.$loaded());
-                }
-                Promise.all(promisses).then(function(values) {
-                    console.log(values); // [3, 1337, "foo"] 
-                    return callback(values);
-                });
-            },
+            substance.similarTo = similarTo;
+        } else {
+            substance.isSimilar = false;
+        }
+        $scope.dataList.$add(substance).then(function() {
+            $("#messages").html("Salvo com sucesso").fadeIn(function() {
+                $(this).fadeOut(3000);
+                $scope.substance = "";
+            });
         });
 
-
-
-    };
-
-    $scope.saveSimilar = function() {
-        console.log(arr);
-        console.log(obj);
-    };
-    $scope.changeSimilar = function() {
-        console.log(arr);
-        console.log(obj);
     };
 
 });
 
-// app.controller("substanciesCrtl", function($scope, $firebaseArray) {
-//     var ref = new Firebase("https://allergyhelper3.firebaseio.com/substancies");
-//     var obj = $firebaseArray(ref);
-//     $scope.dataList = obj;
-
-//     $scope.addSubstance = function() {
-//         var substance = $scope.substance;
-//         substance.lowerCaseName = substance.commonName.toLowerCase();
-//         var arrSimilars = $(".subst-select").val();
-//         var similarTo = {};
-//         if (arrSimilars !== null) {
-//             arrSimilars.forEach(function(item) {
-//                 similarTo[item] = true;
-//             });
-//             substance.similarTo = similarTo;
-//         } else {
-//             substance.isSimilar = false;
-//         }
-//         $scope.dataList.$add(substance);
-//     };
-// });
 
 $(".btn-save").click(function() {
     var element = $(this).parent().closest('tr');
